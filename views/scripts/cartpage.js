f3b17e8668e4a34f03c6e6f7a21a1c8c69c36340
Sam Bailey <sam.bailey@paddle.com>
2021-09-10 15:30:43 +0100

    add quantity update logic

--- views/scripts/cartpage.js
+++ views/scripts/cartpage.js
@@ -26,21 +26,21 @@ function populateCart() {
         product1Count++;
         // make sure it only gets built on the first instance, otherwise update
         if (product1Count === 1) {
-          buildCartItem(cartData[i]);
+          buildCartItem(cartData[i], 1);
         } else if (product1Count > 1) {
           updateCartQuantity(cartData[i].product_id, product1Count);
         }
       } else if (cartData[i].product_id === "PR2") {
         product2Count++;
         if (product2Count === 1) {
-          buildCartItem(cartData[i]);
+          buildCartItem(cartData[i], 2);
         } else if (product2Count > 1) {
           updateCartQuantity(cartData[i].product_id, product2Count);
         }
       } else if (cartData[i].product_id === "PR3") {
         product3Count++;
         if (product3Count === 1) {
-          buildCartItem(cartData[i]);
+          buildCartItem(cartData[i], 3);
         } else if (product3Count > 1) {
           updateCartQuantity(cartData[i].product_id, product3Count);
         }
@@ -50,7 +50,7 @@ function populateCart() {
   console.log(product1Count, product2Count, product3Count);
 }
 
-function buildCartItem(product) {
+function buildCartItem(product, coundID) {
   const cartItem = document.createElement("div");
   cartItem.innerHTML = `<div class="row border-top border-bottom">
             <div class="row main align-items-center">
@@ -59,8 +59,8 @@ function buildCartItem(product) {
                 <div class="row">${product.description}</div>
             </div>
             <div class="col">
-                <a href="#">-</a><a href="#" class="border" id=${product.product_id}>1</a
-                ><a href="#">+</a>
+                <a href="#" id="minusQuantity${product.product_id}">-</a><a href="#" class="border" id=${product.product_id}>1</a
+                ><a href="#" id="plusQuantity${product.product_id}">+</a>
             </div>
             <div class="col">
                 &euro; ${product.price} <span class="close">&#10005;</span>
@@ -68,6 +68,43 @@ function buildCartItem(product) {
             </div>
             </div>`;
   cartContainer.appendChild(cartItem);
+  const minus = document.getElementById(`minusQuantity${product.product_id}`);
+  minus.addEventListener("click", () => {
+    const quantity = document.getElementById(`${product.product_id}`);
+    console.log(parseInt(quantity.textContent));
+    let productCount = parseInt(quantity.textContent) - 1;
+    if (productCount != 0) {
+      quantity.innerHTML = productCount;
+      console.log(productCount);
+      // set the global variable for the product count
+      if (coundID == 1) {
+        product1Count = productCount;
+      }
+      if (coundID == 2) {
+        product2Count = productCount;
+      }
+      if (coundID == 3) {
+        product3Count = productCount;
+      }
+    }
+  });
+
+  const plus = document.getElementById(`plusQuantity${product.product_id}`);
+  plus.addEventListener("click", () => {
+    const quantity = document.getElementById(`${product.product_id}`);
+    let productCount = parseInt(quantity.textContent) + 1;
+    quantity.innerHTML = productCount;
+    // set the global variable for the product count
+    if (coundID == 1) {
+      product1Count = productCount;
+    }
+    if (coundID == 2) {
+      product2Count = productCount;
+    }
+    if (coundID == 3) {
+      product3Count = productCount;
+    }
+  });
 }
 
 function updateCartQuantity(id, quantity) {
