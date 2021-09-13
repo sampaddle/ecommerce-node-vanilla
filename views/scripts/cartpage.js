0d3ab1bf1b19981e66c688c581b351b8cfa9057a
Sam Bailey <sam.bailey@paddle.com>
2021-09-13 16:11:27 +0100

    reconfigure cart so totals work again with new data structure

--- views/scripts/cartpage.js
+++ views/scripts/cartpage.js
@@ -17,55 +17,49 @@ const finalTotal = document.getElementById("finalTotal");
 // loop through the items in the product array
 
 console.log(cartData);
-let product1Count = 0;
-let product2Count = 0;
-let product3Count = 0;
-
-let product1Price = 0;
-let product2Price = 0;
-let product3Price = 0;
 
 // 1. count up all the instances of a product
 // 2. generate the cart products based on their count
 
 function populateCart() {
-  for (let i = 0; i < cartData.length; i++) {
-    // check that cartData is not empty - swap this to build function
-    if (cartData != []) {
-      // check for instances of first product
-      if (cartData[i].product_id === "PR1") {
-        product1Count++;
-        // make sure it only gets built on the first instance, otherwise update
-        if (product1Count === 1) {
-          buildCartItem(cartData[i], 1);
-          product1Price = cartData[i].price;
-          console.log(product1Price);
-        } else if (product1Count > 1) {
-          updateCartQuantity(cartData[i].product_id, product1Count);
-        }
-      } else if (cartData[i].product_id === "PR2") {
-        product2Count++;
-        if (product2Count === 1) {
-          buildCartItem(cartData[i], 2);
-          product2Price = cartData[i].price;
-          console.log(product2Price);
-        } else if (product2Count > 1) {
-          updateCartQuantity(cartData[i].product_id, product2Count);
-        }
-      } else if (cartData[i].product_id === "PR3") {
-        product3Count++;
-        if (product3Count === 1) {
-          buildCartItem(cartData[i], 3);
-          product3Price = cartData[i].price;
-          console.log(product3Price);
-        } else if (product3Count > 1) {
-          updateCartQuantity(cartData[i].product_id, product3Count);
-        }
-      }
+  if (cartData != []) {
+    for (let i = 0; i < cartData.length; i++) {
+      buildCartItem(cartData[i], cartData[i].product_id);
+      // check that cartData is not empty - swap this to build function
+      //   // check for instances of first product
+      //   if (cartData[i].product_id === "PR1") {
+      //     product1Count++;
+      //     // make sure it only gets built on the first instance, otherwise update
+      //     if (product1Count === 1) {
+      //       buildCartItem(cartData[i], 1);
+      //       product1Price = cartData[i].price;
+      //       console.log(product1Price);
+      //     } else if (product1Count > 1) {
+      //       updateCartQuantity(cartData[i].product_id, product1Count);
+      //     }
+      //   } else if (cartData[i].product_id === "PR2") {
+      //     product2Count++;
+      //     if (product2Count === 1) {
+      //       buildCartItem(cartData[i], 2);
+      //       product2Price = cartData[i].price;
+      //       console.log(product2Price);
+      //     } else if (product2Count > 1) {
+      //       updateCartQuantity(cartData[i].product_id, product2Count);
+      //     }
+      //   } else if (cartData[i].product_id === "PR3") {
+      //     product3Count++;
+      //     if (product3Count === 1) {
+      //       buildCartItem(cartData[i], 3);
+      //       product3Price = cartData[i].price;
+      //       console.log(product3Price);
+      //     } else if (product3Count > 1) {
+      //       updateCartQuantity(cartData[i].product_id, product3Count);
+      //     }
+      //   }
     }
   }
-  calculateTotal();
-  console.log(product1Count, product2Count, product3Count);
+  calculateTotal(cartData);
+  //   console.log(product1Count, product2Count, product3Count);
 }
 
 function buildCartItem(product, countID) {
@@ -77,7 +71,7 @@ function buildCartItem(product, countID) {
                 <div class="row">${product.description}</div>
             </div>
             <div class="col">
-                <a href="#" id="minusQuantity${product.product_id}">-</a><a href="#" class="border" id=${product.product_id}>1</a
+                <a href="#" id="minusQuantity${product.product_id}">-</a><a href="#" class="border" id=${product.product_id}>${product.cart_quantity}</a
                 ><a href="#" id="plusQuantity${product.product_id}">+</a>
             </div>
             <div class="col">
@@ -92,18 +86,23 @@ function buildCartItem(product, countID) {
     let productCount = parseInt(quantity.textContent) - 1;
     if (productCount != 0) {
       quantity.innerHTML = productCount;
-      // set the global variable for the product count
-      if (countID == 1) {
-        product1Count = productCount;
-      }
-      if (countID == 2) {
-        product2Count = productCount;
-      }
-      if (countID == 3) {
-        product3Count = productCount;
-      }
+      product.cart_quantity = productCount;
     }
-    calculateTotal();
+    // if (productCount != 0) {
+    //   quantity.innerHTML = productCount;
+    //   // set the global variable for the product count
+    //   if (countID == 1) {
+    //     product1Count = productCount;
+    //   }
+    //   if (countID == 2) {
+    //     product2Count = productCount;
+    //   }
+    //   if (countID == 3) {
+    //     product3Count = productCount;
+    //   }
+    // }
+    console.log(cartData);
+    calculateTotal(cartData);
   });
 
   const plus = document.getElementById(`plusQuantity${product.product_id}`);
@@ -113,18 +112,19 @@ function buildCartItem(product, countID) {
     let productCount = parseInt(quantity.textContent) + 1;
     // update the DOM
     quantity.innerHTML = productCount;
+    product.cart_quantity = productCount;
     // set the global variable for the product count
-    if (countID == 1) {
-      product1Count = productCount;
-    }
-    if (countID == 2) {
-      product2Count = productCount;
-    }
-    if (countID == 3) {
-      product3Count = productCount;
-    }
+    // if (countID == 1) {
+    //   product1Count = productCount;
+    // }
+    // if (countID == 2) {
+    //   product2Count = productCount;
+    // }
+    // if (countID == 3) {
+    //   product3Count = productCount;
+    // }
     // update the basket total
-    calculateTotal();
+    calculateTotal(cartData);
     // OPTIONAL TODO: need to update the localStorage with this count info.
     // cartTotal
   });
@@ -148,8 +148,8 @@ populateCart();
 //  1. once the cart has finished populating
 //  2. every time the quantity is updated
 // adjust the page with the total price
-
-function calculateTotal() {
+console.log(cartData);
+function calculateTotal(cartData) {
   // information we have access to from producCount variables:
   // quantity of the product
   // existence of the product in the basket (if the productCount > 0)
@@ -160,11 +160,41 @@ function calculateTotal() {
   let product2Total = 0;
   let product3Total = 0;
 
-  product1Total = product1Count * product1Price;
+  let product1Quantity = 0;
+  let product2Quantity = 0;
+  let product3Quantity = 0;
+
+  let product1Price = 0;
+  let product2Price = 0;
+  let product3Price = 0;
+
+  console.log(cartData);
+  for (let i = 0; i < cartData.length; i++) {
+    if (cartData[i].product_id === "PR1") {
+      product1Quantity = cartData[i].cart_quantity;
+      console.log(product1Quantity);
+      product1Price = cartData[i].price;
+      console.log(product1Price);
+    }
+    if (cartData[i].product_id === "PR2") {
+      product2Quantity = cartData[i].cart_quantity;
+      console.log(product2Quantity);
+      product2Price = cartData[i].price;
+      console.log(product2Price);
+    }
+    if (cartData[i].product_id === "PR3") {
+      product3Quantity = cartData[i].cart_quantity;
+      console.log(product3Quantity);
+      product3Price = cartData[i].price;
+      console.log(product3Price);
+    }
+  }
+
+  product1Total = product1Quantity * product1Price;
   console.log(product1Total);
-  product2Total = product2Count * product2Price;
+  product2Total = product2Quantity * product2Price;
   console.log(product2Total);
-  product3Total = product3Count * product3Price;
+  product3Total = product3Quantity * product3Price;
   console.log(product3Total);
   grandTotal = product1Total + product2Total + product3Total;
   grandTotal = round(grandTotal);
